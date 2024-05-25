426db76cb19375655ab9cd952911dab91566c8f8
William Redenbaugh <wmredenbaugh@gmail.com>
2024-05-25 07:26:18 -0700

    debugged display uploading support

--- mirror_client/flash.sh
+++ mirror_client/flash.sh
@@ -1,5 +1,5 @@
-cargo build
-sshpass -p "+Masterclass1879" scp target/armv7-unknown-linux-gnueabihf/debug/hello pi@192.168.1.21:/home/pi/
-sshpass -p "+Masterclass1879" ssh pi@192.168.1.21
+cargo build --target aarch64-unknown-linux-gnu
+sshpass -p "mamaral2000" scp target/aarch64-unknown-linux-gnu/debug/hello wredenba@192.168.3.249:/home/wredenba/
+sshpass -p "mamaral2000" ssh wredenba@192.168.3.249
 
 echo "Build, push and run complete!"
--- mirror_client/src/main.rs
+++ mirror_client/src/main.rs
@@ -27,19 +27,50 @@ struct MatrixAnimationData{
     string_text: String, 
 }
 
-fn handle_incoming_message(data_in: &[u8], matrix: &mut pimeroni_unicornhd::Matrix){
+fn handle_incoming_message(data_in: &[u8], matrix: &mut pimeroni_unicornhd::Matrix, pixel_index: &mut u32){
     let x_index = data_in[0];
     let y_index = data_in[1];
     let len = data_in[2];
 
-    for data_index in 0..len{
-        let arr_index = (data_index as usize * 3 + 3);
-        
-        matrix.set_pixel((x_index + data_index / 16).into(), (y_index +  data_index % 16).into(), 
-            data_in[arr_index], 
-            data_in[arr_index + 1], 
-            data_in[arr_index + 2]);
+    if x_index == 0{
+        for data_index in 0..len{
+            *pixel_index = *pixel_index + 1;
+            let arr_index = data_index as usize * 3 + 3;
+            matrix.set_pixel(*pixel_index / 16, 
+                *pixel_index %16,
+                data_in[arr_index], 
+                data_in[arr_index + 1], 
+                data_in[arr_index + 2]);
+        }
     }
+
+    if(x_index == 1){
+        *pixel_index = 0;
+        for data_index in 0..len{
+            *pixel_index = *pixel_index + 1;
+            let arr_index = data_index as usize * 3 + 3;
+            matrix.set_pixel(*pixel_index / 16, 
+                *pixel_index %16,
+                data_in[arr_index], 
+                data_in[arr_index + 1], 
+                data_in[arr_index + 2]);
+        }
+    }
+
+    if x_index == 2{
+        for data_index in 0..len{
+            *pixel_index = *pixel_index + 1;
+            let arr_index = data_index as usize * 3 + 3;
+            matrix.set_pixel(*pixel_index / 16, 
+                *pixel_index %16,
+                data_in[arr_index], 
+                data_in[arr_index + 1], 
+                data_in[arr_index + 2]);
+        }
+        matrix.update();
+    }
+
+    matrix.update();
 }
 
 
@@ -52,10 +83,11 @@ fn handle_client(mut stream: UdpSocket, matrix_tx: Sender<MatrixAnimationData>) 
     let ten_millis = time::Duration::from_millis(100);
     thread::sleep(ten_millis);
     let mut read = [0; 1028];
+    let mut pixel_index = 0;
     loop {
         match stream.recv_from(&mut read) {
             Ok(n) => {
-                handle_incoming_message(&mut read, &mut matrix)
+                handle_incoming_message(&mut read, &mut matrix, &mut pixel_index)
             }
             Err(err) => {
                 panic!("{}", err);
@@ -67,7 +99,7 @@ fn handle_client(mut stream: UdpSocket, matrix_tx: Sender<MatrixAnimationData>) 
 
 fn main() {
     let (matrix_tx, matrix_rx): (Sender<MatrixAnimationData>, Receiver<MatrixAnimationData>) = mpsc::channel();
-    let socket = UdpSocket::bind("127.0.0.1:34254").unwrap();
+    let socket = UdpSocket::bind("192.168.3.249:34254").unwrap();
     
     let mut matrix_tx_clone = matrix_tx.clone();
     handle_client(socket, matrix_tx_clone);
